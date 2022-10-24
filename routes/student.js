b595c96ebf1656dcc5a12857da393135d213a6a2
gontar <a.k.gontar02@mail.ru>
2022-10-25 01:09:11 +0800

    new

--- routes/student.js
+++ routes/student.js
@@ -2,11 +2,11 @@ var db = require("./database.js");
 var express = require("express");
 // Вызываем функцию Router(), чтобы создать новый объект маршрутизации. Основной уже располагается в app.js
 var router = express.Router();
-
+var isAuth = require('./isAuth');
 // Указание, что модуль является экспортируемым (теперь его можно подключать в другие модули)
 module.exports = router;
 
-router.get("/listStudents", function(req, res)  {
+router.get("/listStudents", isAuth.isAuthenticated, (req, res) => {
     db.all(
         `SELECT student.*, student_group.name as student_group_name FROM student
         INNER JOIN student_group ON student_group.id=student.student_group_id`,
@@ -19,7 +19,7 @@ router.get("/listStudents", function(req, res)  {
             title: "Список студентов"
         });
     });
-});  
+});
 
    // :id — параметр запроса
 router.get("/student/:id", (req, res) => {
